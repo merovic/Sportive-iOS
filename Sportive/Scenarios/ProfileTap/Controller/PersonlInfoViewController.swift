8be64005c9dbedca5f40c0daed19114106a0b79e
Mostafa <mostafatarek15894@gmail.com>
2019-10-28 08:55:16 -0700

    add class profile and personal

---
+++ Sportive/Scenarios/ProfileTap/Controller/PersonlInfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PersonlInfoViewController.swift
+//  Sportive
+//
+//  Created by Mustafa on 10/28/19.
+//  Copyright © 2019 amirahmed. All rights reserved.
+//
+
+import UIKit
+
+class PersonlInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Sportive/Scenarios/ProfileTap/Controller/ProfileViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ProfileViewController.swift
+//  Sportive
+//
+//  Created by Mustafa on 10/28/19.
+//  Copyright © 2019 amirahmed. All rights reserved.
+//
+
+import UIKit
+import MapKit
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

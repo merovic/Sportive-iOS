2019a2b83b920bc8f46960988d1ae5e399f55a4c
3Lilio <alimohamed2019@gmail.com>
2019-10-28 17:58:51 +0200

    adding dummy class for test

---
+++ Sportive/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  Sportive
+//
+//  Created by Ali Mohamed on 10/28/19.
+//  Copyright © 2019 amirahmed. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+// dummy class
+}

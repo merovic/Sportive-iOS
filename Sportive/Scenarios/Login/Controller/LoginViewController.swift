48ce3f9da99b9d87d0434befffce8931d0a4f191
Mostafa <mostafatarek15894@gmail.com>
2019-10-30 11:11:53 +0200

    Edit Login View

--- Sportive/Scenarios/Login/Controller/LoginViewController.swift
+++ Sportive/Scenarios/Login/Controller/LoginViewController.swift
@@ -20,12 +20,31 @@ class LoginViewController: UIViewController,NVActivityIndicatorViewable {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        passwordTF.isSecureTextEntry = true
         // Do any additional setup after loading the view.
     }
     
-
     //MARK - IBAction
+
+    @IBAction func emailAction(_ sender: UITextField) {
+        let emailValid = vaildateEmail(emailStr: sender.text!)
+        if emailValid {
+            successVaildateTextField(textField: sender)
+        }else {
+            rongVaildateTextField(textField: sender)
+        }
+ 
+    }
+    
+    @IBAction func passwordAction(_ sender: UITextField) {
+        if sender.text!.count < 8 {
+                       rongVaildateTextField(textField: sender)
+                   }else {
+                       successVaildateTextField(textField: sender)
+                   }
+ 
+    }
+    
 
     @IBAction func logInButtonPressed(_ sender: UIButton) {
         login ()
@@ -47,23 +66,16 @@ class LoginViewController: UIViewController,NVActivityIndicatorViewable {
 
                         }
                     }
-               
-                
             
         }
         
     }
-    
    
-    func login ()
-    {
-        
+    func login (){
         self.startAnimating()
-        
         DispatchQueue.main.async {
             // Test Login request
-            
-            if let email = self.yourEmailTf.text , let password = self.passwordTF.text {
+            if let email = self.yourEmailTf.text,!email.isEmpty , let password = self.passwordTF.text,!password.isEmpty {
                 APIClient.loginUser(email:email ,password:password,completion: { result in
                             switch result {
                             case .success(let response):
@@ -79,8 +91,43 @@ class LoginViewController: UIViewController,NVActivityIndicatorViewable {
                                 self.stopAnimating()
                             }
                         })
+            }else{
+                
+                self.rongVaildateTextField(textField: self.yourEmailTf)
+                self.rongVaildateTextField(textField: self.passwordTF)
+                self.stopAnimating()
+ 
             }
-        
+            
         }
     }
+    
+    func rongVaildateTextField(textField : UITextField)  {
+        textField.layer.borderWidth = 4
+        textField.layer.borderColor = #colorLiteral(red: 0.5807225108, green: 0.066734083, blue: 0, alpha: 1)
+    }
+    
+    func successVaildateTextField(textField : UITextField)  {
+        textField.layer.borderWidth = 4
+        textField.layer.borderColor = #colorLiteral(red: 0, green: 0.5628422499, blue: 0.3188166618, alpha: 1)
+    }
+    
+    
+    func vaildateEmail(emailStr : String) -> Bool {
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegex)
+        return emailPred.evaluate(with: emailStr)
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
+
